c432445cd7a7567987e27d022f23b43f5bad91f8
Shlok Sharma <shlokrma@gmail.com>
2026-07-07 16:20:52 -0700

    set up database

---
+++ shared/database.py
@@ -0,0 +1,18 @@
+
+import os
+from pathlib import Path
+
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import (
+    DeclarativeBase,
+    Mapped,
+    mapped_column,
+    sessionmaker
+)
+
+load_dotenv(Path(__file__).resolve().parents[1] / ".env")
+database_password = os.getenv("DATABASE_PASSWORD") or ""
+
+DATABASE_URL = f"postgresql://postgres:{database_password}@localhost:5432/osipress"
+engine = create_engine(DATABASE_URL, echo=True)
