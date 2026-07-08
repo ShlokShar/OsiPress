30293fbc8087f6641f857732775713edddb3ae7c
Shlok Sharma <shlokrma@gmail.com>
2026-07-07 18:45:31 -0700

    basic database setup

--- shared/database.py
+++ shared/database.py
@@ -6,13 +6,19 @@ from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import (
     DeclarativeBase,
-    Mapped,
-    mapped_column,
-    sessionmaker
+    sessionmaker,
 )
+
 
 load_dotenv(Path(__file__).resolve().parents[1] / ".env")
 database_password = os.getenv("DATABASE_PASSWORD") or ""
 
 DATABASE_URL = f"postgresql://postgres:{database_password}@localhost:5432/osipress"
-engine = create_engine(DATABASE_URL, echo=True)
+engine = create_engine(DATABASE_URL, echo=False)
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+SessionLocal = sessionmaker(bind=engine)
--- shared/models.py
+++ shared/models.py
@@ -1,9 +1,55 @@
 
+from sqlalchemy import ForeignKey
 from sqlalchemy.orm import (
-    DeclarativeBase,
     Mapped,
     mapped_column,
-    sessionmaker
 )
 
+from shared.database import Base, SessionLocal
 
+
+class Countries(Base):
+    __tablename__ = 'countries'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column()
+
+    @classmethod
+    def get_country(cls, name: str) -> Mapped[str]:
+        with SessionLocal() as session:
+            return session.query(cls).filter(cls.name == name).first()
+
+
+class Sources(Base):
+    __tablename__ = 'sources'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    country_id: Mapped[int] = mapped_column(ForeignKey('countries.id'))
+    name: Mapped[str] = mapped_column()
+    link: Mapped[str] = mapped_column()
+    political_leaning: Mapped[str] = mapped_column()
+
+    @classmethod
+    def get_source(cls, country_id: int) -> Mapped[int]:
+        with SessionLocal() as session:
+            return session.query(cls).filter(cls.country_id == country_id).first()
+
+
+class Articles(Base):
+    __tablename__ = 'articles'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    source_id: Mapped[int] = mapped_column(ForeignKey('sources.id'))
+    headline: Mapped[str] = mapped_column()
+    translated_headline: Mapped[str] = mapped_column()
+    link: Mapped[str] = mapped_column()
+    summary: Mapped[str] = mapped_column()
+    references_original: Mapped[str] = mapped_column()
+    references_translated: Mapped[str] = mapped_column()
+
+
+    @staticmethod
+    def add_article(article):
+        with SessionLocal() as session:
+            session.add(article)
+            session.commit()
