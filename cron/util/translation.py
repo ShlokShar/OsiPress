fc54ce28d39f9bf777fa0855dcc9b7a6191a3bbc
Shlok Sharma <shlokrma@gmail.com>
2026-07-07 19:19:40 -0700

    added function to convert reference lists

--- cron/util/translation.py
+++ cron/util/translation.py
@@ -1,5 +1,9 @@
 
 from deep_translator import GoogleTranslator
 
-def translate(text):
+def translate(text: str) -> str:
     return GoogleTranslator(source="auto", target="en").translate(text)
+
+
+def translate_references(references: list) -> list:
+    return [translate(reference) for reference in references]
