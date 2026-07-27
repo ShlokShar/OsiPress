4d701610cc960bbcd84552fadf8bbd5488318613
Shlok Sharma <shlokrma@gmail.com>
2026-07-27 09:54:23 -0700

    added backfill script to embed any non-indexed articles

---
+++ cron/util/backfill.py
@@ -0,0 +1,37 @@
+
+from sqlalchemy import (
+    select,
+    func
+)
+
+from cron.util.search_service import SearchService
+from shared.database import SessionLocal
+from shared.models import (
+    Articles
+)
+
+
+def backfill() -> None:
+    search_service = SearchService()
+
+    # grab articles that aren't embedded
+    with SessionLocal() as session:
+        statement = (
+            select(Articles)
+            .where(Articles.embedding.is_(None))
+        )
+        unembedded_articles = session.execute(statement).scalars().all()
+
+        # iterate through unembedded articles
+        for article in unembedded_articles:
+
+            # embed each unembedded article
+            text_to_embed = article.embedding_text()
+            embedding = search_service.embed(text_to_embed)
+            article.embedding = embedding
+            session.add(article)
+            session.commit()
+
+
+if __name__ == "__main__":
+    backfill()
